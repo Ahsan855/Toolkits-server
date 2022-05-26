c0614d2cabdc3323ceac43ca68ab1ee557fbb35b
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-05-26 16:09:54 +0600

    done id fatch

--- index.js
+++ index.js
@@ -78,12 +78,7 @@ const run = async () => {
       res.send(result);
     });
 
-    app.delete("/order/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await orderCollection.deleteOne(query);
-      res.send(result);
-    });
+    
     app.delete("/products/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
