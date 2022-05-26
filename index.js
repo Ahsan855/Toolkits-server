20c00e77d9080a120350fae69a6bab4c443ba919
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-05-26 16:10:16 +0600

    done delete api

--- index.js
+++ index.js
@@ -78,7 +78,12 @@ const run = async () => {
       res.send(result);
     });
 
-    
+    app.delete("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
     app.delete("/products/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
